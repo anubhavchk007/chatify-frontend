f256642a16d5fefcc1912761080cef47cfac0322
anubhavchk007 <anubhavclasher2262@gmail.com>
2025-06-21 17:14:23 +0530

    replaced BrowserRouter with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter} from 'react-router'
+import { BrowserRouter, HashRouter} from 'react-router'
 import AppRoutes from './config/MyRoutes.jsx'
 import { ChatProvider } from './context/ChatContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <Toaster />
     <ChatProvider>
       <AppRoutes />
     </ChatProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
