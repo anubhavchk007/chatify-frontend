586566db64651e3f323fa2249cb2e20394c8b9b8
anubhavchk007 <anubhavclasher2262@gmail.com>
2025-06-21 16:50:34 +0530

    edited baseURL for backend

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = 'http://localhost:8080';
+export const baseURL = 'https://chatify-backend-0qps.onrender.com';
 
 export const httpClient = axios.create({
     baseURL: baseURL,
